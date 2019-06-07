3965f6b37c6641eff019d59c29b877810b625eb5
Sidharth Nayyar <sidnayyar1@gmail.com>
2019-06-06 21:03:23 -0400

    BasicCalculator

---
+++ Day1Sample/main.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  Day1Sample
+//
+//  Created by MacStudent on 2019-06-06.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+let 🐒 = "monkey"
+
+
+
+var a = 10;
+var b = 20;
+var add = 0;
+var divide = 0;
+var multiply = 0;
+var subtract = 0;
+
+add = a+b;
+multiply = a*b;
+divide = a/b;
+subtract = a-b;
+
+print("Addition is :\(add)")
+print("multiplication is :\(multiply)")
+print("division is :\(divide)")
+print("subtraction is :\(subtract)")
